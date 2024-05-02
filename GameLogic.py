e4a41bdb85d9ebeb3d7b042ae878fdb918f60187
RMalone8 <131809387+RMalone8@users.noreply.github.com>
2024-05-02 16:32:00 +0200

    Flushes Are Counted

--- GameLogic.py
+++ GameLogic.py
@@ -1,6 +1,7 @@
 import random
 import math
 import numpy as np
+from itertools import chain, combinations
 
 deck = [ # Spades
         {"title": "A",
@@ -271,39 +272,42 @@ deck = [ # Spades
          "order_value": 13},
          ]
 
-def generateHand() -> list:
+def generateCards(num_cards: int) -> list:
     hand = []
-    for i in range(5): # Will be 6
+    for i in range(num_cards):
         hand.append(deck.pop(random.randint(0,len(deck)-1)))
     return hand
 
-player1 = generateHand()
-#player2 = generateHand()
+player1 = generateCards(4)
+#player2 = generateCards()
+cut_card = generateCards(1)
 
-print(f"Player one has the following hand:")
-for card in player1:
-    print(card['suit'] + " " + card['title'])
-
-def point_check(hand: list) -> int:
+def point_check(hand: list, cut_card: dict, crib: bool = False) -> int:
     total_points = 0
     run_multiplier = 1
 
+    hand_w_cut = hand + cut_card
+
+    print(f"The following hand is:")
+    for card in hand_w_cut:
+        print(card['suit'] + " " + card['title'])
+
     # Card face value
-    values = [card['value'] for card in hand]
+    values = [card['value'] for card in hand_w_cut]
     values.sort()
 
     # Card order value
-    order_values = [card['order_value'] for card in hand]
+    order_values = [card['order_value'] for card in hand_w_cut]
     order_values.sort()
     order_values_set = list(sorted(set(order_values)))
 
     # Getting points for pairs
     pairs = [order_values.count(num) for num in order_values_set]
-    total_points += sum([num*(num-1) for num in pairs if num > 1])
+    total_points += sum([num*(num-1) for num in pairs])
 
     print(f"Points after pairs: {total_points}")
-    
-    # Getting points for runs
+
+    # Getting points for runs - rigged in a way to work for all 5-card hands: only keeps track on the highest run in a hand
     ordered_differences = np.array([order_values_set[i+1] - order_values_set[i] for i in range(len(order_values_set)) if i < len(order_values_set) - 1])
     runs = np.split(ordered_differences, np.where(ordered_differences > 1)[0])
     counter = max([sum(run==1) for run in runs]) + 1
@@ -319,8 +323,14 @@ def point_check(hand: list) -> int:
     print(f"Points after fifteens: {total_points}")
 
     # Getting points for a flush
-        # I honestly don't remember how much this is worth
+    if len(set([card["suit"] for card in hand])) == 1 and hand[0]["suit"] == cut_card[0]["suit"]:
+        total_points += 5
+    elif len(set([card["suit"] for card in hand])) == 1 and not crib:
+        total_points += 4
+
+    print(f"Points after flushes: {total_points}")
 
     return total_points
     
-point_check(player1)
+
+point_check(hand= player1, cut_card= cut_card)
