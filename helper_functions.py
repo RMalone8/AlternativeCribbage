fc3826f0e6d3b2c586d364b71280023d79178a79
RMalone8 <131809387+RMalone8@users.noreply.github.com>
2025-03-15 16:28:17 -0400

    Update helper_functions.py
    The screen shows the counting phase now and can count the points in the players' hands at the click of a button

--- helper_functions.py
+++ helper_functions.py
@@ -305,50 +305,78 @@ def check_for_playable_cards(hand: list, running_sum: int) -> bool:
             return True                      
     return False
 
-def draw_hand(win, hand: list, discard_list: list) -> list:
+def draw_hand(win, hand: list, discard_list: list, finished_peggging: bool) -> list:
     '''
     Draws the hand of the active player, returns the position of the cards
     '''
+    #print(hand)
     i = 0
     pos_info = []
-    for card in hand:
-        # cards being displayed
-        # for the sprites that are being used, to keep proper proportions: height = 1.38411 * width
-        pos_dict = {"x": i*198 + 3, "y": 800//2 + 110, "width": 200, "height": 280}
-        pos_info.append(pos_dict)
-        card_sprite = pygame.image.load(f"Assets/Cards/Modern/{(card['suit'][0]).lower()}{card['order_value']}.png").convert_alpha()
-        card_sprite = pygame.transform.scale(card_sprite, (pos_dict['width'], pos_dict['height']))
-        if i in discard_list:
-            highlighted_sprite = pygame.Surface(card_sprite.get_size()).convert_alpha()
-            highlighted_sprite.fill((255,255,160))
-            card_sprite.blit(highlighted_sprite, (0,0), special_flags=pygame.BLEND_RGBA_MULT)
-        card_rect = card_sprite.get_rect()
-        card_rect.topleft = (pos_dict['x'], pos_dict['y'])
-        win.blit(card_sprite, card_rect)
-        i += 1
+    # setting up the hand pre-pegging
+    if not finished_peggging:
+        for card in hand:
+            # cards being displayed.
+            # for the sprites that are being used, to keep proper proportions: height = 1.38411 * width
+            pos_dict = {"x": i*198 + 3, "y": 800//2 + 110, "width": 200, "height": 280}
+            pos_info.append(pos_dict)
+            card_sprite = pygame.image.load(f"Assets/Cards/Modern/{(card['suit'][0]).lower()}{card['order_value']}.png").convert_alpha()
+            card_sprite = pygame.transform.scale(card_sprite, (pos_dict['width'], pos_dict['height']))
+            if i in discard_list:
+                highlighted_sprite = pygame.Surface(card_sprite.get_size()).convert_alpha()
+                highlighted_sprite.fill((255,255,160))
+                card_sprite.blit(highlighted_sprite, (0,0), special_flags=pygame.BLEND_RGBA_MULT)
+            card_rect = card_sprite.get_rect()
+            card_rect.topleft = (pos_dict['x'], pos_dict['y'])
+            win.blit(card_sprite, card_rect)
+            i += 1
+    else: # setting up the hand post-pegging
+        for card in hand:
+            # cards being displayed
+            pos_dict = {"x": i*198 + 3, "y": 200, "width": 200, "height": 280}
+            pos_info.append(pos_dict)
+            card_sprite = pygame.image.load(f"Assets/Cards/Modern/{(card['suit'][0]).lower()}{card['order_value']}.png").convert_alpha()
+            card_sprite = pygame.transform.scale(card_sprite, (pos_dict['width'], pos_dict['height']))
+            card_rect = card_sprite.get_rect()
+            card_rect.topleft = (pos_dict['x'], pos_dict['y'])
+            win.blit(card_sprite, card_rect)
+            i += 1
     return pos_info
 
-def draw_game_objs(win, player_piles: list) -> list:
+def draw_game_objs(win, player_piles: list, cut_card: dict, finsihed_pegging: bool) -> list:
     '''
     Drawing game objects (buttons, graphics) and 
     returning the positions of the interactable ones
     '''
-    # The 'go' button
     pos_info = []
-    pos_dict = {"x": 80, "y": 800//2 - 160, "width": 120, "height": 60}
-    pos_info.append(pos_dict)
-    pygame.draw.rect(win, (173, 216, 230), (pos_dict['x'], pos_dict['y'], pos_dict['width'], pos_dict['height']))
-    # each player's pegging pile
-    i = 0
-    for pile in player_piles:
-        for card in pile:
-            pile_card_sprite = pygame.image.load(f"Assets/Cards/Modern/{(card['suit'][0]).lower()}{card['order_value']}.png").convert_alpha()
-            pile_card_sprite = pygame.transform.scale(pile_card_sprite, (100, 140))
-            pile_card_sprite = pygame.transform.rotate(pile_card_sprite, card['rotation'])
-            pile_card_rect = pile_card_sprite.get_rect()
-            pile_card_rect.topleft = (1000, 150*i + 100)
-            win.blit(pile_card_sprite, pile_card_rect)
-        i += 1
+    # setting up the screen pre-pegging
+    if not finsihed_pegging:
+        # The 'go' button
+        pos_dict = {"x": 80, "y": 800//2 - 160, "width": 120, "height": 60}
+        pos_info.append(pos_dict)
+        pygame.draw.rect(win, (173, 216, 230), (pos_dict['x'], pos_dict['y'], pos_dict['width'], pos_dict['height']))
+        # each player's pegging pile
+        i = 0
+        for pile in player_piles:
+            for card in pile:
+                pile_card_sprite = pygame.image.load(f"Assets/Cards/Modern/{(card['suit'][0]).lower()}{card['order_value']}.png").convert_alpha()
+                pile_card_sprite = pygame.transform.scale(pile_card_sprite, (100, 140))
+                pile_card_sprite = pygame.transform.rotate(pile_card_sprite, card['rotation'])
+                pile_card_rect = pile_card_sprite.get_rect()
+                pile_card_rect.topleft = (1000, 150*i + 100)
+                win.blit(pile_card_sprite, pile_card_rect)
+            i += 1
+        # drawing the cut card if we've cut it
+        if cut_card is not None:
+            cut_card_sprite = pygame.image.load(f"Assets/Cards/Modern/{(cut_card['suit'][0]).lower()}{cut_card['order_value']}.png").convert_alpha()
+            cut_card_sprite = pygame.transform.scale(cut_card_sprite, (150, 210))
+            cut_card_rect = cut_card_sprite.get_rect()
+            cut_card_rect.topleft = (800, 200)
+            win.blit(cut_card_sprite, cut_card_rect)
+    else: # setting up the screen post-pegging
+        # The 'go' button
+        pos_dict = {"x": 400, "y": 600, "width": 120, "height": 60}
+        pos_info.append(pos_dict)
+        pygame.draw.rect(win, (173, 216, 230), (pos_dict['x'], pos_dict['y'], pos_dict['width'], pos_dict['height']))
     return pos_dict
 
 def check_click(x: float, y: float, pos_info: list, num_cards: int) -> int: # encoding for which button
@@ -371,10 +399,10 @@ def is_in_bounds(x: float, y: float, positions: list) -> bool:
         return True
     return False
 
-def discarding_logic(select: int, discard_list: list, crib: list, player_hand: list):
+def discarding_logic(select: int, discard_list: list, crib: list, player_hand: list) -> int:
     '''
     Logic for selecting two of the cards in hands to be discarded
-    Returns whose turn it is
+    Returns an int to either increment the turn or stay where we are
     '''
     if select < 6:
         # deselecting
@@ -420,3 +448,12 @@ def pegging_logic(selection: int, player_hands: list, player_points: list, turn:
             running_sum = 0
         turn += 1
     return go_count, turn, running_sum, new_points, #pile, player_piles, player_hands, new_points
+
+def counting_logic(selection: int, player_hand: list, player_points: list, turn: int, is_crib: bool = False) -> int:
+    '''
+    Returns an int to either increment the turn or stay where we are
+    '''
+    if selection == 6:
+        player_points[turn%2] += point_check(player_hand, is_crib=is_crib)
+        return 1
+    return 0
