8ebdd06de278f245e20ced40d8771e16dd78bcf3
RMalone8 <131809387+RMalone8@users.noreply.github.com>
2025-04-25 15:00:26 -0400

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,19 +1,21 @@
 import pygame
 
 class Button:
-    def __init__(self, color, x, y, height=100, width=100):
+    def __init__(self, color, x, y, label:str, height=100, width=100, enabled=False):
         self.color = color
         self.x = x
         self.y = y
+        self.label = label
         self.height = height
         self.width = width
+        self.enabled = enabled
+        self.show_label = False
 
     def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        if self.enabled:
+            pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
 
     def check_click(self, mouse_x, mouse_y) -> bool:
-        if self.x < mouse_x < self.x + self.width and self.y < mouse_y < self.y + self.height:
-            return True
-        return False
-
-    
+        if self.x < mouse_x < self.x + self.width and self.y < mouse_y < self.y + self.height and self.enabled:
+            return self.label
+        return ""
